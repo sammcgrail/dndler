61c1382a42d6a1f3fdb23008aed648c3b9b685c8
Varun Nadgir <theminivann@gmail.com>
2021-09-24 19:43:47 -0400

    Update generate_all.py

--- generate_all.py
+++ generate_all.py
@@ -1,12 +1,23 @@
+# import other generators
 import generate_race
 import generate_name
 import generate_class
 import generate_background
 import generate_stats
 
+# master generator
 def generate_all():
-    generate_race()
-    generate_name()
-    generate_class()
-    generate_background()
-    generate_stats()
+    # empty character dict that gets filled in by individual generators
+    char_dict = {"race":["", []],
+                 "name":"",
+                 "class":"",
+                 "background":["", {"trait":"", "ideal":"", "bond":"", "flaw":""}],
+                 "stats":[]}
+    char_dict["race"] = generate_race()
+    char_dict["name"] = generate_name()
+    char_dict["class"] = generate_class()
+    char_dict["background"] = generate_background()
+    char_dict['stats'] = generate_stats()
+
+    # return filled character dict
+    return char_dict
