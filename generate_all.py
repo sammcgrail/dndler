cc0f6d79bd56293d24cf18c10a3645d53f2652e1
Varun Nadgir <theminivann@gmail.com>
2021-09-30 20:03:32 -0400

    Update generate_all.py
    fixed generator

--- generate_all.py
+++ generate_all.py
@@ -6,14 +6,14 @@ import generate_background
 import generate_stats
 
 # master generator
-def generate_all(race_choice='any', background_choice='any'):
+def generate_all(background_choice='any'):
     # empty character dict that gets filled in by individual generators
     char_dict = {'race':'',
                  'name':'',
                  'class':'',
                  'background':{},
                  'stats':{}}
-    char_dict['race'] = generate_race(race_choice)
+    char_dict['race'] = generate_race()
     char_dict['name'] = generate_name()
     char_dict['class'] = generate_class()
     char_dict['background'] = generate_background(background_choice)
