5667d54fd13de8c5c668ed30eec047959b8575e2
br-ndt <nlax76@gmail.com>
2021-12-10 16:25:44 -0500

    add js for button reactivity

---
+++ public/js/mouseOver.js
@@ -0,0 +1,22 @@
+let mouseDown = 0;
+window.onload = mouseWatch = () => {
+    document.body.onmousedown = () => {
+        mouseDown = 1;
+    }
+    
+    document.body.onmouseup = () => {
+        mouseDown = 0;
+    }
+
+    document.body.onmouseleave = () => {
+        mouseDown = 0;
+    }
+}
+   
+mouseOver = (id) => {
+    if(mouseDown)
+    {
+        document.getElementById(id).checked = 1-document.getElementById(id).checked;
+        document.getElementById(id).dispatchEvent(new Event('input'));
+    }
+}
