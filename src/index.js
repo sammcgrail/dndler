e14ee3acb29e685dd6ed28fdca5ab8780c89cab7
br-ndt <nlax76@gmail.com>
2022-01-23 14:32:46 -0500

    update express routing

--- src/index.js
+++ src/index.js
@@ -1,20 +1,29 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const { body, validationResult } = require('express-validator');
 
+
 const port = process.env.PORT || 3000;
 
-app.use(express.static(__dirname + '/..'));
+app.use(express.static(__dirname));
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.status(200).sendFile(__dirname + '../index.html');
+    res.status(200).sendFile(path.join(__dirname, '/index.html'));
+})
+
+app.get('/custom', (req, res) => {
+    res.status(200).sendFile(path.join(__dirname, '/custom.html'));
 });
 
 app.post('/', (req, res) => {
-    Character.create({
+    res.status(200).send('A randomized character will show up here.');
+    let dataToSend;
+});
 
-    })
+app.post('/custom.html', (req, res) => {
+    res.status(200).send('A character reflecting the chosen filters will show up here.');
     let dataToSend;
 });
 
